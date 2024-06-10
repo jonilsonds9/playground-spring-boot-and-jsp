af8d39dce095e807cfaf983c1b26c2e711d6684a
Jonilson Sousa <jonilsonds9@gmail.com>
2024-06-10 11:10:53 -0300

    Adicionado conexao ao redis

--- src/main/java/br/com/jonilson/playground/PlaygroundApplication.java
+++ src/main/java/br/com/jonilson/playground/PlaygroundApplication.java
@@ -1,13 +1,24 @@
 package br.com.jonilson.playground;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
-public class PlaygroundApplication {
+public class PlaygroundApplication implements CommandLineRunner {
+
+	@Autowired
+	private RedisTemplate<String, String> redisTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlaygroundApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		redisTemplate.opsForValue().set("myKey", "myValue");
+		System.out.println("Value from redis: " + redisTemplate.opsForValue().get("myKey"));
+	}
 }
